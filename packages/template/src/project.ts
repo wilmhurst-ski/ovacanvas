b05767a9991f19b050b206af3cba5632600c6022
wilmhurst-ski <268077635+wilmhurst-ski@users.noreply.github.com>
2026-09-20 18:44:17 +0100

    feat(template): add derivative scene examples

--- packages/template/src/project.ts
+++ packages/template/src/project.ts
@@ -1,8 +1,8 @@
 import {makeProject} from '@ovacanvas/core';
 
-import example from './scenes/example?scene';
+import derivatives from './scenes/derivatives?scene';
 
 export default makeProject({
   experimentalFeatures: true,
-  scenes: [example],
+  scenes: [derivatives],
 });
